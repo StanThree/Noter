073d5bb50737e0a8bd03bb0d0b207dc495ca6508
Tristan Epler <107583597+StanThree@users.noreply.github.com>
2024-02-19 14:59:28 -0500

    Changed config to use windows postgresql

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://tristan:thethousand@localhost/noterdb'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:thethousand@localhost/noterdev'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
